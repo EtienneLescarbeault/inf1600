d6949d245f179b8d40c1bb7b4f430aeadbf7ffe2
Etienne Lescarbeault <etienne.lescarbeault@polymtl.ca>
2019-03-05 15:38:58 -0500

    Ajout de equal.s complété avec succès

--- inf1600/TP3/equals.s
+++ inf1600/TP3/equals.s
@@ -4,32 +4,46 @@ matrix_equals_asm:
         push %ebp			/* Save old base pointer */
         mov %esp, %ebp		/* Set ebp to current esp */
 
-        sub $8, %esp			# Alloue espace dans la pile pour r et c, les indices row/column.
-		mov $0, -4(%ebp)		# r = 0 	
-        mov $0, -8($ebp)		# c = 0
+		sub $12, %esp   #espace alloué pour r et c
 
-								# Les paramètres sont stockés dans les registres
-		mov 8(%ebp), %eax 		# %eax <- inmatdata1
-		mov 12(%ebp), %ecx		# %ecx <- inmatdata2
-		mov 16(%ebp), %edx		# %edx <- matorder
+		movl $0, -8(%ebp)		#r=0
+		movl $0, -12(%ebp)		#c=0
+
+		mov 8(%ebp), %esi		#esi <- inmatdata1
+		mov 12(%ebp), %edi		#edi <- inmatdata2
+		mov 16(%ebp), %ebx		#ebx <- matorder
 
 forR:
-
-forC:	
-		# Instruction if(inmatdata1[c + r * matorder] != inmatdata2[c + r * matorder])
+forC:
+		mov -8(%ebp), %eax		#eax <- r
+		mul %edx				#eax <-edx * eax, revient à: eax = r * matorder
+		addl -12(%ebp), %eax    #eax = c + r* matorder
+		mov %eax, (%esi)
+		mov %eax, (%edi)
+		cmp %esi, %edi
+		jne retZero
 
 continueForC:
-		inc -8(%ebp)			# c++
-		cmp -8(%ebp), %edx		# flags = matorder - c
-		ja forC					# Si c ­< matorder on exécute la boucle forC 
-		mov $0, -8(%ebp)		# Sinon, on réinitialise l'indice c = 0
+		incl -12(%ebp) #c++
+		cmp -12(%ebp), %ebx
+		ja forC
+		movl $0, -12(%ebp)
 
 continueForR:
-		inc -4(%ebp)			# r++
-		cmp -4(%ebp), %edx		# flags = matorder - r
-		ja forR					# Si r ­< matorder on exécute la boucle forR
-		mov $0, -4(%ebp)		# Sinon, on réinitialise l'indice r = 0		
+		incl -8(%ebp)
+		cmp -8(%ebp), %ebx
+		ja forR
+		#mettre r à 0?
 
+retUn:
+		mov $1, %eax
+		jmp epilogue
+	
+retZero:
+		mov $0, %eax
+		jmp epilogue
 
+epilogue:
+		mov -4(%ebp), %ebx
         leave          /* Restore ebp and esp */
         ret            /* Return to the caller */
