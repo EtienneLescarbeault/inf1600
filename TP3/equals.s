0fc9804f1ce83c2c73876d195c2aeecf3d897f22
Etienne Lescarbeault <etienne.lescarbeault@polymtl.ca>
2019-03-12 12:54:15 -0400

    Ajout TP4 et réorganisation du repos

--- TP3/equals.s
+++ TP3/equals.s
@@ -1,50 +1,49 @@
 .globl matrix_equals_asm
 
 matrix_equals_asm:
-								# Prologue
-        push %ebp				# Sauvegarde l'ancien base pointer sur la pile
-        mov %esp, %ebp			# Sauvegarde le %esp courant dans %ebp
-		push %ebx				# Sauvegarde le contenu du registre %ebx sur la pile
+    push %ebp			/* Save old base pointer */
+    mov %esp, %ebp		/* Set ebp to current esp */
 
-        sub $12, %esp			# Alloue espace dans la pile pour r et c, les indices row/column.
-		mov $0, -8(%ebp)		# r = 0 	
-        mov $0, -12(%ebp)		# c = 0
+	sub $8, %esp   				#espace alloué dans la pile pour r et c, deux entiers de 4 octets
 
-								# Les paramètres sont stockés dans les registres
-		mov 8(%ebp), %ebx 		# %ebx <- inmatdata1
-		mov 12(%ebp), %ecx		# %ecx <- inmatdata2
-		mov 16(%ebp), %edx		# %edx <- matorder
+	movl $0, -4(%ebp)			#r=0 afin d'initialiser la boucle forR
+	#c est dans -8(%ebp)
+	mov 8(%ebp), %esi			#esi = inmatdata1, 4 octets
+	mov 12(%ebp), %edi			#edi = inmatdata2, 4 octets
+	mov 16(%ebp), %ebx			#ebx = matorder, 4 octets
+
+forR:							#label de la boucle for avec r < matorder
+	movl $0, -8(%ebp)	    	#On initialise c à 0 avant le début de la boucle forC
+
+forC:							#label de la boucle for avec c < matorder
+	mov -4(%ebp), %eax	   		#eax = r
+	imul %ebx, %eax		    	#eax = r * matorder
+	addl -8(%ebp), %eax     	#eax = c + r* matorder
+	lea (, %eax,4), %edx    	#edx = eax*4
+	lea (%edx, %esi), %esi  	#esi = inmatdata1+4*(c+r*matorder), revient à inmatdata1[c+r*matorder]
+	lea (%edx, %edi), %edi  	#edi = inmatdata2 + 4*(c+r*matorder), revient à inmatdata2[c+r*matorder]
+
+	cmp %esi, %edi		    	#on compare les deux pointeurs
+	jne retZero			    	#S'ils ne sont pas égaux, on retourne 0 (voir retZero)
 		
-forR:
-forC:	
-								# Instruction if(inmatdata1[c+r*matorder]!=inmatdata2[c+r*matorder])
-		mov -8(%ebp), %eax		# %eax = r
-		mul %edx				# %eax = r * matorder
-		add -12(%ebp), %eax		# %eax = c + r * matorder
-		cmp (%eax, %ebx), (%eax, %ecx) 
-								# inmatdata2[c+r*matorder] - inmatdata1[c+r*matorder]
-		jne	returnZero			# Saute à returnZero si des éléments des matrices ne sont pas identiques
+continueForC:					#label pour la continuité de la boucle for avec c (la condition d'arrêt n'a pas été remplie)       
+	cmp -8(%ebp), %ebx			#Comparaison entre la valeur de c et celle de matorder
+	incl -8(%ebp)				#On incrémente la valeur de c (++c)
+	ja forC						#Si matorder est plus grand que c, on refait la boucle (retour vers forC)
+	
+continueForR:					#label pour la continuité de la boucle for avec r (la condition d'arrêt n'a pas été remplie)
+	cmp -4(%ebp), %ebx			#Comparaison entre r et matorder
+	incl -4(%ebp)				#On incrémente la valeur de r (++r)
+	ja forR						#Si matorder est plus grand que r, on refait la boucle. Sinon, aucun branchement n'est à faire et on sort.
 
-continueForC:
-		inc -12(%ebp)			# c++
-		cmp -12(%ebp), %edx		# flags = matorder - c
-		ja forC					# Si c ­< matorder on exécute la boucle forC 
-		mov $0, -12(%ebp)		# Sinon, on réinitialise l'indice c = 0
+retUn:							#Label pour retourner 1 en cas d'égalité des deux matrices
+	mov $1, %eax				#On donne la valeur 1 au registre eax
+	jmp epilogue				#Branchement vers l'épilogue
+	
+retZero:						#Label pour retourner 0 en cas d'inégalité des matrices
+	mov $0, %eax				#Donne la valeur 0 au registre eax
+	jmp epilogue				#Branchement vers l'épilogue
 
-continueForR:
-		inc -8(%ebp)			# r++
-		cmp -8(%ebp), %edx		# flags = matorder - r
-		ja forR					# Si r ­< matorder on exécute la boucle forR
-		mov $0, -8(%ebp)		# Sinon, on réinitialise l'indice r = 0		
-
-returnOne:
-		mov $1, %eax			# %eax = 1, les matrices sont équivalentes
-		jmp epilogue			# Saute à l'épilogue de la fonction
-
-returnZero:
-		mov $0, %eax			# %eax = 0, les matrices sont différentes
-		
-epilogue:						# Épilogue
-		mov -4(%ebp), %ebx		# Ré-établie ebx
-        leave					# Ré-établie ebp et esp d'origine
-        ret						# Retour à l'appelant de la fonction
+epilogue:						#Label pour la fin du programme
+    leave          				/* Restore ebp and esp */
+    ret            				/* Return to the caller */
