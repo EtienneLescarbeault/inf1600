d6aa684111c09d07e74a1b9d19b88a1aca9eb025
Etienne Lescarbeault <etienne.lescarbeault@polymtl.ca>
2019-03-21 22:14:22 -0400

    Triangle_area.s terminé

--- TP4/triangle_area.s
+++ TP4/triangle_area.s
@@ -1,3 +1,7 @@
+.data
+        diviseur: .float 2.0
+
+.text
 .globl _ZNK9CTriangle7AreaAsmEv
 
 _ZNK9CTriangle7AreaAsmEv:
@@ -5,6 +9,45 @@ _ZNK9CTriangle7AreaAsmEv:
         mov %esp, %ebp /* set ebp to current esp */
         
         /* Write your solution here */
+        subl $8, %esp
+        movl 8(%ebp), %eax
+        push %eax               #this
+        movl (%eax), %eax       #vtable
+        addl $8, %eax           #appel de la fonction assembleur du périmètre, 2e dans la vtable
+        call *(%eax)            #Calcul du périmètre du triangle
         
+        movl $diviseur, %edx
+        push %edx
+        fld (%edx)              #(%edx) dans st[0]
+        fdivrp                  #Le périmètre est divisé par 2
+
+        fstp -8(%ebp)           #Le résultat va dans p
+
+        movl 8(%ebp), %eax      #array de côtés va dans eax
+        fld 4(%eax)             #Premier côté dans st[0]
+        fld -8(%ebp)
+        fsubp                  #p-side[0] dans st[0]
+        fstp (%edx)
+
+        fld 8(%eax)             #side[1] dans st[0]
+        fld -8(%ebp)            #p dans st[1]
+        fsubp                  #p-side[1] dans st[0]
+        fld (%edx)
+        fmulp
+        fstp (%edx)
+
+        fld 12(%eax)
+        fld -8(%ebp)
+        fsubp
+        fld (%edx)
+        fmulp
+        
+        fld -8(%ebp)
+        fmulp
+        fsqrt
+
+        addl $8, %esp
+        
+epilogue:
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
