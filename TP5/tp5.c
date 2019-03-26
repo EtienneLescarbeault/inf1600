21f9e073c784992eb7736aa7842e160fc0ceae8b
julefb <jules.lefebvre@polymtl.ca>
2019-03-26 14:50:46 -0400

    Avancement de exo 1 avec rotate.

--- TP5/tp5.c
+++ TP5/tp5.c
@@ -6,18 +6,21 @@ unsigned int Decryption_fct(unsigned int le)
 	unsigned int be;
 
 	
-	 // Remplacez le code suivant par de l'assembleur en ligne
-	 // en utilisant le moins d'instructions possible
-	 
-	 be = (le & 0xff000000) | (le&0xff) << 16  | (le & 0xff00) | (le & 0xff0000) >> 16;	
+	// Remplacez le code suivant par de l'assembleur en ligne
+	// en utilisant le moins d'instructions possible 
+	// be = (le & 0xff000000) | (le & 0xff) << 16  | (le & 0xff00) | (le & 0xff0000) >> 16;	
 
-	/* asm volatile (
-		// instructions...
-		: // sorties (s'il y a lieu)
-		: // entrées
-		: // registres modifiés (s'il y a lieu)
+	asm volatile (
+		"movl %1, %%ecx\n\t"			// %ecx = le = ee aa bb ff
+		"andl $0x00ff00ff, %%ecx\n\t"	// %ecx = 00 aa 00 ff
+		"rol $16, %%ecx\n\t"			// %ecx = 00 ff 00 aa
+		"movl %1, %0\n\t"				// be = ee aa bb ff
+		"andl $0xff00ff00, %0\n\t"		// be = ee 00 bb 00
+		"orl %%ecx, %0\n\t"				// be = ee ff bb aa
+		: "=r"(be)			// Pas de sorties
+		: "r"(le)			// entrées
+		: "ecx"		 		// registres modifiés
 	);
-	*/
 
 	return be;
 }
