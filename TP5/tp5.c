f15446dd1e0e678699cceacf6ae6aedf1a1ed826
julefb <jules.lefebvre@polymtl.ca>
2019-03-26 15:03:33 -0400

    Exo 1 terminé

--- TP5/tp5.c
+++ TP5/tp5.c
@@ -11,7 +11,7 @@ unsigned int Decryption_fct(unsigned int le)
 	// be = (le & 0xff000000) | (le & 0xff) << 16  | (le & 0xff00) | (le & 0xff0000) >> 16;	
 
 	asm volatile (
-		"movl %1, %%ecx\n\t"			// %ecx = le = ee aa bb ff
+		/*"movl %1, %%ecx\n\t"			// %ecx = le = ee aa bb ff
 		"andl $0x00ff00ff, %%ecx\n\t"	// %ecx = 00 aa 00 ff
 		"rol $16, %%ecx\n\t"			// %ecx = 00 ff 00 aa
 		"movl %1, %0\n\t"				// be = ee aa bb ff
@@ -20,6 +20,13 @@ unsigned int Decryption_fct(unsigned int le)
 		: "=r"(be)			// Pas de sorties
 		: "r"(le)			// entrées
 		: "ecx"		 		// registres modifiés
+*/	
+		"rol $8, %1\n\t"	// le = aa bb ff ee
+		"bswap %1\n\t"		// le = ee ff bb aa
+		"movl %1, %0\n\t"	// be = le		
+		: "=r"(be)			// Sorties
+		: "r"(le)			// Entrées
+		:			 		// Registres modifiés
 	);
 
 	return be;
